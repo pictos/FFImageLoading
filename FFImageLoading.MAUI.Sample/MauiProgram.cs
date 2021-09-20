24d6d98bf77cab0db8741507369482864b6dab56
Pedro Jesus <pedrojesus.cefet@gmail.com>
2021-09-20 00:37:21 -0300

    Maui App running Android version

---
+++ FFImageLoading.MAUI.Sample/MauiProgram.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Maui;
+using Microsoft.Maui.Controls.Compatibility;
+using Microsoft.Maui.Controls.Hosting;
+using Microsoft.Maui.Hosting;
+
+namespace FFImageLoading.MAUI.Sample
+{
+	public static class MauiProgram
+	{
+		public static MauiApp CreateMauiApp()
+		{
+			var builder = MauiApp.CreateBuilder();
+			builder
+				.UseMauiApp<App>()
+				.ConfigureFonts(fonts =>
+				{
+					fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+				})
+				.ConfigureMauiHandlers(h =>
+				{
+#if ANDROID
+					h.AddCompatibilityRenderer(typeof(FFImageLoading.Forms.CachedImage), typeof(FFImageLoading.Forms.Platform.CachedImageRenderer));
+#endif
+				});
+			return builder.Build();
+		}
+	}
+}
